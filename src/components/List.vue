35e0e5bd9ce9e7dda187eac06fd839715a50d5da
Infant Frontender <newbornfrontender@gmail.com>
2018-10-27 19:47:14 +0300

    Добавил компоненты списков

---
+++ src/components/List.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+  name: 'List',
+  props: {
+    tag: {
+      type: String,
+      required: true,
+      validator: (value) => {
+        const ol = value === 'ol';
+        const ul = value === 'ul';
+        return ol || ul;
+      },
+    },
+    items: {
+      type: Array,
+      required: true,
+    },
+  },
+  render(h) {
+    return h(
+      this.tag,
+      this.items.map((item) => {
+        return h('li', [
+          h(
+            'router-link',
+            {
+              attrs: {
+                to: item.path,
+              },
+            },
+            item.title,
+          ),
+        ]);
+      }),
+    );
+  },
+};
+</script>
---
+++ src/components/NavList.vue
@@ -0,0 +1,31 @@
+<script>
+import List from './List';
+
+export default {
+  name: 'NavList',
+  components: {
+    List,
+  },
+  data: () => ({
+    items: [
+      {
+        title: 'Home',
+        path: '/',
+      },
+      {
+        title: 'About',
+        path: '/about',
+      },
+    ],
+  }),
+};
+</script>
+
+<template>
+  <nav>
+    <List
+      :tag="'ul'"
+      :items="items"
+    />
+  </nav>
+</template>
