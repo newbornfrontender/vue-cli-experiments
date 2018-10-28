d372148072916cf6301f6f7f2621515f2fe0cfde
Infant Frontender <newbornfrontender@gmail.com>
2018-10-28 18:58:50 +0300

    Добавил страницу с постами

---
+++ src/components/PostPreview.vue
@@ -0,0 +1,149 @@
+<script>
+export default {
+  name: 'PostPreview',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+    imageHeight: {
+      type: Number,
+      default: 150,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+    imagePosition: {
+      type: String,
+      default: 'center',
+    },
+    preview: {
+      type: String,
+      required: false,
+    },
+    tags: {
+      type: Array,
+      default: () => [],
+    },
+  },
+};
+</script>
+
+<template>
+  <article :class="$style.post">
+    <h2 :class="$style.title">{{ title }}</h2>
+
+    <div
+      :class="$style.tagWrapper"
+      v-if="tags.length"
+    >
+      <span
+        :class="$style.tag"
+        v-for="tag of tags"
+        :key="tag"
+      >
+        {{ tag }}
+      </span>
+    </div>
+
+    <figure :class="$style.imageWrapper">
+      <figcaption :class="$style.imageLabel">{{ label }}</figcaption>
+
+      <div
+        :class="$style.image"
+        :style="{
+          height: `${imageHeight}px`,
+          backgroundImage: `url(${imageUrl})`,
+          backgroundPosition: imagePosition,
+        }"
+      />
+    </figure>
+
+    <p :class="$style.previewText">{{ preview }}</p>
+
+    <p v-if="$slots.default">
+      <slot />
+    </p>
+  </article>
+</template>
+
+<style module>
+.post {
+  background-color: #607d8b;
+  border-radius: 20px;
+  transform: scale(0.98);
+  box-shadow: 0px 2px 3px #3d5561;
+}
+
+.post:not(:last-child) {
+  margin-bottom: 12px;
+}
+
+.post:hover {
+  transform: scale(1);
+  box-shadow: 0px 3px 10px 1px #3d5561;
+  transition-duration: 0.2s;
+}
+
+.title {
+  margin: 0;
+  padding: 10px 0;
+  font-size: 30px;
+  text-align: center;
+  color: #30434c;
+  text-shadow: 1px 1px #607d8b, 2px 2px #30434c;
+}
+
+.tagWrapper {
+  display: flex;
+  justify-content: flex-end;
+  padding: 5px 0;
+}
+
+.tag {
+  margin-right: 4px;
+  padding: 2px;
+  background-color: #9E9E9E;
+  color: #47484c;
+  border-radius: 3px;
+  cursor: pointer;
+}
+
+.tag:hover {
+  color: white;
+}
+
+.imageWrapper {
+  position: relative;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+.imageLabel {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 2px 0;
+  background-color: rgba(0, 0, 0, 0.4);
+  color: white;
+  text-align: center;
+}
+
+.image {
+  width: 100%;
+  background-size: 100%;
+  cursor: pointer;
+}
+
+.previewText {
+  margin: 0;
+  padding: 10px;
+  font-size: 16px;
+  color: white;
+}
+</style>
---
+++ src/views/Posts.vue
@@ -0,0 +1,51 @@
+<script>
+import PostPreview from '@/components/PostPreview';
+
+export default {
+  name: 'Posts',
+  components: {
+    PostPreview,
+  },
+  data: () => ({
+    title: 'Posts',
+    posts: [
+      {
+        title: 'How to write code',
+        label: 'PHP code in Sublime Text',
+        imageHeight: 200,
+        imageUrl:
+          'http://www.lumete.it/wp-content/uploads/2016/09/code-944499_960_720.jpg',
+        tags: ['PHP', 'CSS', 'HTML', 'Code', 'Sublime Text 2', 'Introduction', 'Beginners'],
+        preview: 'In this article I will tell you how to write code correctly',
+      },
+      {
+        title: 'Test',
+        label: 'Test',
+        imageUrl:
+          'http://www.lumete.it/wp-content/uploads/2016/09/code-944499_960_720.jpg',
+        tags: ['Test'],
+        preview: 'Test',
+      },
+    ],
+  }),
+};
+</script>
+
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+
+    <section>
+      <PostPreview
+        v-for="{ title, label, alt, imageHeight, imageUrl, preview, tags } of posts"
+        :key="title"
+        :title="title"
+        :label="label"
+        :imageHeight="imageHeight"
+        :imageUrl="imageUrl"
+        :preview="preview"
+        :tags="tags"
+      />
+    </section>
+  </div>
+</template>
