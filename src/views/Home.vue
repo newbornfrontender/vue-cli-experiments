b995d0f7ac0a4f1357a0715c98520e71012b5a66
Infant Frontender <newbornfrontender@gmail.com>
2018-10-29 13:43:18 +0300

    Убрал компонент Fragment со страницы, т.к. перестал работать роутинг

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,13 @@
 <script>
-import { Fragment } from 'vue-fragment';
-
 export default {
   name: 'Home',
-  components: {
-    Fragment,
-  },
 };
 </script>
 
 <template>
-  <fragment>
+  <section>
     <h1>Home</h1>
+
     <p>This is home page</p>
-  </fragment>
+  </section>
 </template>
